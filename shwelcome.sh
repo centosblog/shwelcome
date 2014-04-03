f4ac039266a5f1e47adb4b916f5f246f9ac408b1
Curtis K <contact@centosblog.com>
2014-04-03 15:09:55 +1100

    Added quotation marks to prevent filename globbing.

--- shwelcome.sh
+++ shwelcome.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Get the working directory of this script.
-SCRIPT_DIRECTORY=$( dirname $0 )
+SCRIPT_DIRECTORY="$( dirname $0 )"
 
 # Includes file
-. ${SCRIPT_DIRECTORY}/includes/includes.sh
+. "${SCRIPT_DIRECTORY}/includes/includes.sh"
 
 # Load modules with the load_module function.
 # Modules are printed out in the order that they are loaded here.
